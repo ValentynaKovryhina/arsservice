909260efe7922e9e748b0958a3f914c5304f0152
Valentyna Kovryhina <valentynakovryhina@gmail.com>
2024-10-03 01:34:41 +0200

    Mobile styles

--- admin/partials/ars-service-admin-info-page.php
+++ admin/partials/ars-service-admin-info-page.php
@@ -31,52 +31,54 @@
 
         </div>
 
-        <table id="ars_orders_list" class="widefat striped">
-            <thead>
-            <tr>
-                <th class="manage-column">Id</th>
-                <th class="manage-column"><?php _e( 'Дата создания', 'ars-service' ); ?></th>
-                <th class="manage-column"><?php _e( 'Устройство', 'ars-service' ); ?></th>
-                <th class="manage-column">S/N</th>
-                <th class="manage-column"><?php _e( 'Стоимость', 'ars-service' ); ?></th>
-                <th class="manage-column"><?php _e( 'Статус', 'ars-service' ); ?></th>
-                <th class="manage-column"><?php _e( 'Телефон', 'ars-service' ); ?></th>
-                <th class="manage-column"></th>
-                <th class="manage-column"></th>
-            </tr>
-            </thead>
-
-            <tbody>
-
-			<?php foreach ( $services as $service ) : ?>
-                <tr data-id="<?php echo mb_strtolower( $service['id'] ); ?>"
-                    data-sn="<?php echo mb_strtolower( $service['sn'] ); ?>"
-                    data-phone="<?php echo isset( $service['phone'] ) && $service['phone'] ? mb_strtolower( $service['phone'] ) : ''; ?>">
-                    <td><?php echo $service['id']; ?></td>
-                    <td><?php echo $service['date']; ?></td>
-                    <td><?php echo $service['device']; ?></td>
-                    <td><?php echo $service['sn']; ?></td>
-                    <td><?php echo isset( $service['price'] ) && $service['price'] ? $service['price'] . '&nbsp;&euro;' : ''; ?></td>
-                    <td><?php echo isset( $statuses[ $service['status'] ] ) && $statuses[ $service['status'] ] ? $statuses[ $service['status'] ] : ''; ?></td>
-                    <td><?php echo isset( $service['phone'] ) && $service['phone'] ? $service['phone'] : ''; ?></td>
-
-                    <td>
-                        <a href="<?php echo admin_url( 'admin.php?page=ars-create-order&ars-order=' . $service['id'] ); ?>">
-                            <span class="dashicons dashicons-edit"></span>
-                        </a>
-                    </td>
-
-                    <td>
-                        <a href="#" class="ars_delete_item" data-delete-id="<?php echo $service['id']; ?>">
-                            <span class="dashicons dashicons-trash"></span>
-                        </a>
-                    </td>
-
+        <div class="ars_table_wrapper">
+            <table id="ars_orders_list" class="widefat striped">
+                <thead>
+                <tr>
+                    <th class="manage-column">Id</th>
+                    <th class="manage-column"><?php _e( 'Дата создания', 'ars-service' ); ?></th>
+                    <th class="manage-column"><?php _e( 'Устройство', 'ars-service' ); ?></th>
+                    <th class="manage-column">S/N</th>
+                    <th class="manage-column"><?php _e( 'Стоимость', 'ars-service' ); ?></th>
+                    <th class="manage-column"><?php _e( 'Статус', 'ars-service' ); ?></th>
+                    <th class="manage-column"><?php _e( 'Телефон', 'ars-service' ); ?></th>
+                    <th class="manage-column"></th>
+                    <th class="manage-column"></th>
                 </tr>
-			<?php endforeach; ?>
+                </thead>
 
-            </tbody>
-        </table>
+                <tbody>
+
+				<?php foreach ( $services as $service ) : ?>
+                    <tr data-id="<?php echo mb_strtolower( $service['id'] ); ?>"
+                        data-sn="<?php echo mb_strtolower( $service['sn'] ); ?>"
+                        data-phone="<?php echo isset( $service['phone'] ) && $service['phone'] ? mb_strtolower( $service['phone'] ) : ''; ?>">
+                        <td><?php echo $service['id']; ?></td>
+                        <td><?php echo $service['date']; ?></td>
+                        <td><?php echo $service['device']; ?></td>
+                        <td><?php echo $service['sn']; ?></td>
+                        <td><?php echo isset( $service['price'] ) && $service['price'] ? $service['price'] . '&nbsp;&euro;' : ''; ?></td>
+                        <td><?php echo isset( $statuses[ $service['status'] ] ) && $statuses[ $service['status'] ] ? $statuses[ $service['status'] ] : ''; ?></td>
+                        <td><?php echo isset( $service['phone'] ) && $service['phone'] ? $service['phone'] : ''; ?></td>
+
+                        <td>
+                            <a href="<?php echo admin_url( 'admin.php?page=ars-create-order&ars-order=' . $service['id'] ); ?>">
+                                <span class="dashicons dashicons-edit"></span>
+                            </a>
+                        </td>
+
+                        <td>
+                            <a href="#" class="ars_delete_item" data-delete-id="<?php echo $service['id']; ?>">
+                                <span class="dashicons dashicons-trash"></span>
+                            </a>
+                        </td>
+
+                    </tr>
+				<?php endforeach; ?>
+
+                </tbody>
+            </table>
+        </div>
 
 	<?php else : ?>
 
